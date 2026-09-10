36ae25ed81e6e718edb28ee2c14b6d3878ce6e1f
eendychan <endy.endygg@gmail.com>
2026-09-10 06:33:48 +0300

    Update DialogState.kt

--- app/src/main/kotlin/com/flxrs/dankchat/ui/main/dialog/DialogState.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/ui/main/dialog/DialogState.kt
@@ -1,6 +1,7 @@
 package com.flxrs.dankchat.ui.main.dialog
 
 import androidx.compose.runtime.Immutable
+import com.flxrs.dankchat.data.UserName
 import com.flxrs.dankchat.data.repo.crash.CrashEntry
 
 @Immutable
@@ -8,7 +9,9 @@ data class DialogState(
     val showAddChannel: Boolean = false,
     val showManageChannels: Boolean = false,
     val showRemoveChannel: Boolean = false,
+    val removeChannelTarget: UserName? = null,
     val showBlockChannel: Boolean = false,
+    val blockChannelTarget: UserName? = null,
     val showLogout: Boolean = false,
     val showNewWhisper: Boolean = false,
     val pendingUploadAction: (() -> Unit)? = null,
